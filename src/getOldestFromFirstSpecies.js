329cdbc6059198291b8f0d48a52dfeed36b32acf
caiooliveirah09 <leelros@hotmail.com>
2022-05-26 07:22:55 -0300

    i solved requirement 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const responsableFor = data.employees.find((employe) => employe.id === id).responsibleFor[0];
+  const oldest = data.species.find((specie) =>
+    specie.id === responsableFor).residents.reduce((acc, curr) =>
+    (curr.age > acc ? curr.age : acc), 0);
+  const oldestObject = data.species.find((specie) =>
+    specie.id === responsableFor).residents.find((resident) =>
+    resident.age === oldest);
+  return [oldestObject.name, oldestObject.sex, oldestObject.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
